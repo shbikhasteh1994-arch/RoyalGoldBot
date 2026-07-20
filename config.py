bc3c10c091fa5accf4dcfe4b46f59803c50018cc
shbikhasteh1994-arch <s.h.bikhasteh1994@gmail.com>
2026-07-20 06:49:08 +0330

    Update environment variable usage for bot credentials

--- config.py
+++ config.py
@@ -1,4 +1,6 @@
 @RoyalGold_Shopbot
 
-BOT_TOKEN = os.getenv("8893002935:AAFSlR7vYtty8VXxe_Tr7-AUeGZ4fv1DCy4")
-CHANNEL_ID = os.getenv("@RoyalGold_Shopbot")
+import os
+
+BOT_TOKEN = os.getenv("BOT_TOKEN")
+CHANNEL_ID = os.getenv("CHANNEL_ID")
